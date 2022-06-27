830e7a76e1973add2c12599e6721538d9f78d5b9
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2022-06-27 17:50:55 +0200

    Create Server Using HTTP

---
+++ Node.js/HTTPS/js/app.js
@@ -0,0 +1,15 @@
+import RunNode from "./lessons/01 run-node/run.js";
+import CreateNewModule from "./lessons/02 create-server/server.js";
+import Routing from "./lessons/02 create-server/route.js";
+import Express from "./lessons/03 express/express.js";
+
+// ============================ Intro ============================
+// RunNode();
+
+// ======================== Create Server ========================
+// This Is The Simple Way To Create Server
+// CreateNewModule();
+// Routing();
+
+// =========================== Express ===========================
+Express();
