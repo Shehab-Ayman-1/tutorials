66b139c232e567ac4882d4d8c1e7c63af747df6f
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2024-01-04 21:37:40 +0200

    New_Commit

--- Next/app/page.tsx
+++ Next/app/page.tsx
@@ -1,3 +1,17 @@
+import { Typography } from "@/components/ui";
+import Link from "next/link";
+
 export default async function Home() {
-   return <h1 className="text-4xl text-white">Home Page !</h1>;
+   return (
+      <div className="fixed-center flex-center flex-col">
+         <img src="./favicon.ico" alt="favicon" />
+         <Typography className="text-gradient text-4xl">Welcome To Next.js Toturial</Typography>
+         <Link className="hover:text-white" href="https://nextjs.org">
+            https://nextjs.org
+         </Link>
+         <Link className="hover:text-white" href="https://next-auth.js.org">
+            https://next-auth.js.org
+         </Link>
+      </div>
+   );
 }
